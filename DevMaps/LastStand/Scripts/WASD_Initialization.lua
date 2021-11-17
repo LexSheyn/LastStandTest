d3f7dd9f65260b3e2159e9922b370adb78cc7074
LexSheyn <tiadec@gmail.com>
2021-11-17 22:00:42 +0300

    Does not work

--- DevMaps/LastStand/Scripts/WASD_Initialization.lua
+++ DevMaps/LastStand/Scripts/WASD_Initialization.lua
@@ -1,9 +1,54 @@
-m_MovementTrigger = CreateTrigger()
+KEY = {
 
-TriggerAddAction(m_MovementTrigger, CheckKeyPress)
+    FORWARD  = OSKEY_W,
+    BACKWARD = OSKEY_S,
+    LEFT     = OSKEY_A,
+    RIGHT    = OSKEY_D
 
-function CheckKeyPress(key)
+}
 
-    BlzTriggerRegisterPlayerKeyEvent(m_MovementTrigger, GetLocalPlayer(), key, 0, true)
+METAKEY = {
+
+    RUN = OSKEY_LSHIFT,
+    SIT = OSKEY_LCONTROL
+
+}
+
+function CreateKeyListener()
+
+    print("Creating Key Listener")
+
+    m_KeyListenerTrigger = CreateTrigger()
+
+    function CheckKeyPressed()
+        
+        BlzTriggerRegisterPlayerKeyEvent(m_KeyListenerTrigger, GetLocalPlayer(), OSKEY_W, 0, true)
+        BlzTriggerRegisterPlayerKeyEvent(m_KeyListenerTrigger, GetLocalPlayer(), OSKEY_W, OSKEY_LSHIFT, true)
+
+        local key_pressed = BlzGetTriggerPlayerKey()
+
+        print("OSKey: ", key_pressed)
+
+        local meta_key_pressed = BlzGetTriggerPlayerMetaKey()
+
+        print("MetaKey: ", meta_key_pressed)    
+
+        if (key_pressed == OSKEY_W) and (meta_key_pressed == 0) then
+
+            SetUnitAnimationByIndex(m_PlayerOne, 26)
+
+        elseif (key_pressed == OSKEY_W) and (meta_key_pressed == OSKEY_LSHIFT) then
+
+            SetUnitAnimationByIndex(m_PlayerOne, 6)
+
+        end        
+
+    end
+
+    TriggerAddAction(m_KeyListenerTrigger, CheckKeyPressed)
+
+    
+
+    print("Key Listener has created")
 
 end
